ab4a296e98be806fae5d1d4398ca745a3068424f
David Neumann <davidjneumann@gmail.com>
2023-11-13 23:05:51 -0500

    Disable auto centering on %

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,7 @@
 
 -- Jump and center
 local centerOpts = { noremap = true, silent = true }
-local mappings = { "<C-u>", "<C-d>", "{", "}", "N", "n", "<C-i>", "<C-o>", "%", "*", "#" }
+local mappings = { "<C-u>", "<C-d>", "{", "}", "N", "n", "<C-i>", "<C-o>", "*", "#" }
 
 for _, key in ipairs(mappings) do
   vim.api.nvim_set_keymap("n", key, key .. "zz", centerOpts)
