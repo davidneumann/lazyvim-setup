2ca5d4b2816c885fe6872cf9196c86207c9c9633
David Neumann <davidjneumann@gmail.com>
2023-11-21 15:32:47 -0500

    Remove mostly not working center on jump maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Jump and center
-local centerOpts = { noremap = true, silent = true }
-local mappings = { "<C-u>", "<C-d>", "{", "}", "N", "n", "<C-i>", "<C-o>", "*", "#" }
-
-for _, key in ipairs(mappings) do
-  vim.api.nvim_set_keymap("n", key, key .. "zz", centerOpts)
-end
+-- -- Jump and center
+-- local centerOpts = { noremap = true, silent = true }
+-- local mappings = { "<C-u>", "<C-d>", "{", "}", "N", "n", "<C-i>", "<C-o>", "*", "#" }
+--
+-- for _, key in ipairs(mappings) do
+--   vim.api.nvim_set_keymap("n", key, key .. "zz", centerOpts)
+-- end
 
 -- Auto center after search
 vim.api.nvim_create_autocmd("CmdLineLeave", {
